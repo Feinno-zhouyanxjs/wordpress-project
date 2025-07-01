46131b5ee9d6ddbf14e8b543f825bd59039059a1
computerProgramer <benben123_456@163.com>
2025-07-01 13:16:47 +0800

    revise stateless source code

--- wordpress/wp-content/themes/twentytwentyfive/functions.php
+++ wordpress/wp-content/themes/twentytwentyfive/functions.php
@@ -156,23 +156,3 @@ if ( ! function_exists( 'twentytwentyfive_format_binding' ) ) :
 		}
 	}
 endif;
-
-add_filter('wp_stateless_file_name', 'hudaring_fix_stateless_file_path', 10, 4);
-function hudaring_fix_stateless_file_path($filename, $post_id, $metadata, $args) {
-    if (!$post_id) return $filename;
-
-    $post = get_post($post_id);
-    if (!$post || $post->post_type !== 'attachment') return $filename;
-
-    // Extract file's upload timestamp
-    $upload_date = $post->post_date; // actual upload time
-
-    $year = date('Y', strtotime($upload_date));
-    $month = date('m', strtotime($upload_date));
-
-    // Replace custom placeholders
-    $filename = str_replace('%file_date_year%', $year, $filename);
-    $filename = str_replace('%file_date_month%', $month, $filename);
-
-    return $filename;
-}
